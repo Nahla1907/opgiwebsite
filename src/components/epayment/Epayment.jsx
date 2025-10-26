bb0b52f71328716f336422ac6fe0951a01250eef
Nahla1907 <johndoughdoug@gmail.com>
2025-10-26 09:52:51 +0100

    Updated project with latest modifications

--- src/components/epayment/Epayment.jsx
+++ src/components/epayment/Epayment.jsx
@@ -4,9 +4,9 @@ import "./Epayment.css"; // custom CSS for animation
 function Epayment() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900">
-      <h1 className="water-text text-6xl font-extrabold text-white">
+      {/* <h1 className="water-text text-6xl font-extrabold text-white">
         Coming Soon
-      </h1>
+      </h1> */}
     </div>
   );
 }
