08dab1ec47b14d942f98efd76d6a668d0ba525ee
Nahla1907 <johndoughdoug@gmail.com>
2025-10-27 16:37:36 +0100

    Added new pics in our rojects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,67 +1,60 @@
-import React from 'react'
-import './Header.css'
-// import {BiMenuAltRight} from 'react-icons/bi'
+import React, { useEffect, useState } from 'react';
+import './Header.css';
 import { CgMenuRound } from "react-icons/cg";
-import  { useEffect, useState } from 'react';
-import OutsideClickHandler from "react-outside-click-handler"
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
-  const [menuOpened, setMenuOpened] = useState(false)
-
-  const getMenuStyles = (menuOpened) => {
-    if(document.documentElement.clientWidth <= 800)
-    {
-      return {right: !menuOpened && "-100%"}
-    }
-  }
-
+  const [menuOpened, setMenuOpened] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
+  // ✅ toggle menu position for mobile
+  const getMenuStyles = () => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: menuOpened ? "0" : "-100%" };
+    }
+    return {}; // normal layout for desktop
+  };
+
+  // ✅ detect scroll (only affects navbar appearance)
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
     };
-
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <section className={`h-wrapper ${isScrolled ? 'scrolled' : ''}`}>
-        <div className='flexCenter paddings innerWidth h-container'>
-            <img src="./logo11.png" alt="logo" width={100}/>
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="./logo11.png" alt="logo" width={80} />
 
-            <OutsideClickHandler
-             onOutsideClick={()=> {
-              setMenuOpened(false)
-            }}
-            >
+        {/* ✅ Close when clicking outside */}
+        <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+          <div
+            className="flexCenter h-menu"
+            style={getMenuStyles()}
+          >
+            <a href="#home" onClick={() => setMenuOpened(false)}>Accueil</a>
+            <a href="#projects" onClick={() => setMenuOpened(false)}>Projet</a>
+            <a href="#annonces" onClick={() => setMenuOpened(false)}>Annonces</a>
+            <a href="#contact" onClick={() => setMenuOpened(false)}>Contact</a>
+            <button className="button" onClick={() => setMenuOpened(false)}>
+              <a href="">E-payment</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
 
-            <div className='flexCenter h-menu'
-            style={getMenuStyles(menuOpened)}
-            >
-              
-               <a href="#home">Accueil</a>
-              <a href="#projects">Projet</a>
-              <a href="#annonces">Annonces</a>
-              <a href="#contact">Contact</a>
-                <button className='button'>
-                    <a href=""> E-payment </a>
-
-                </button>
-
-            </div>
-            </OutsideClickHandler>
-            <div className='menu-icon' onClick={()=>setMenuOpened((prev)=>!prev)}>
-          {/* <BiMenuAltRight size={30}/> */}
-          <CgMenuRound    size={55}/>
-
+        {/* ✅ Toggle open/close on click */}
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <CgMenuRound size={55} />
         </div>
-
-        </div>
-        
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -6,7 +6,7 @@ const data = [
     heading: "PROJECT NUMBER 1",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
-      image: "./djanet main.jpg"
+      image: "./djanet4.jpg"
   },
   {
     icon: <HiShieldCheck/>,
@@ -14,7 +14,7 @@ const data = [
     heading: "PROJECT NUMBER 2",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
-      image: "./hero-image.png"
+      image: "./djanet 2.jpg"
   },
   {
     icon: <HiShieldCheck/>,
@@ -22,7 +22,55 @@ const data = [
     heading: "PROJECT NUMBER 3",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
-      image: "./r3.png"
+      image: "./djanet3.jpg"
+  },
+   {
+    icon: <HiShieldCheck/>,
+    // icon: <MdAnalytics />,
+    heading: "PROJECT NUMBER 3",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      image: "./djanet3.jpg"
+  },
+   {
+    icon: <HiShieldCheck/>,
+    // icon: <MdAnalytics />,
+    heading: "PROJECT NUMBER 3",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      image: "./djanet3.jpg"
+  },
+   {
+    icon: <HiShieldCheck/>,
+    // icon: <MdAnalytics />,
+    heading: "PROJECT NUMBER 3",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      image: "./djanet3.jpg"
+  },
+   {
+    icon: <HiShieldCheck/>,
+    // icon: <MdAnalytics />,
+    heading: "PROJECT NUMBER 3",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      image: "./djanet3.jpg"
+  },
+   {
+    icon: <HiShieldCheck/>,
+    // icon: <MdAnalytics />,
+    heading: "PROJECT NUMBER 3",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      image: "./djanet3.jpg"
+  },
+   {
+    icon: <HiShieldCheck/>,
+    // icon: <MdAnalytics />,
+    heading: "PROJECT NUMBER 3",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      image: "./djanet3.jpg"
   },
   
 ];
